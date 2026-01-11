8165015dc9c5ca91fdf82d0aa7ccd9c89f167de9
Mariusz <casmar90@gmail.com>
2026-01-11 19:10:14 +0100

    refactor and seed more content, small changes

--- src/db/content.ts
+++ src/db/content.ts
@@ -11,7 +11,19 @@ export const content = async () => {
 
   await createUser("Mariusz", hashedPassword, "Mariusz Ciastoń", true, true);
 
+  await createUser("John", hashedPassword, "John C", false, true);
+
+  await createUser("Charlotte", hashedPassword, "Charlotte E", false, false);
+
   await createMessage("Hello!", "This is the first test message.", 1);
 
   await createMessage("Welcome", "Welcome to my Members Only app.", 1);
+
+  await createMessage("Nice!", "Keep up the good work.", 2);
+
+  await createMessage(
+    "Lorem ipsum",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor..",
+    3,
+  );
 };
